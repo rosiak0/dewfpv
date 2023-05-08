d678e7cd0c3954d3f8d3659ea7f326d7133c5796
rosiak0 <maciejrosa1@gmail.com>
2023-05-08 10:20:27 +0200

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import PageTitle from "../components/PageTitle";
 import LanguageContext from "../context/LanguageContext";
 import { useContext } from "react";
 
+const experience = new Date().getFullYear() - 2017;
+
 export default function Home() {
   const context = useContext(LanguageContext);
   return (
@@ -14,8 +16,8 @@ export default function Home() {
       <TextWrapper>
         <p className="md:text-base text-sm">
           {context.siteLanguage === "en"
-            ? "Welcome to Dew Aerials, where we specialize in FPV drone videography. With 6 years of experience, we deliver stunning shots for commercial, real estate, and action sports videos. Our passion and cutting-edge techniques push the boundaries of what's possible. Choose Dew Aerials and take your vision to new heights."
-            : "Witaj w Dew Aerials, specjaliści w dziedzinie wideofilmowania za pomocą dronów FPV. Posiadamy 6-letnie doświadczenie w branży, dostarczając oszałamiające ujęcia dla filmów komercyjnych, nieruchomości oraz sportów ekstremalnych. Nasza pasja oraz innowacyjne techniki pozwalają nam przekraczać granice możliwości. Wybierz Dew Aerials i osiągnij nowe wysokości."}
+            ? `Welcome to Dew Aerials, where we specialize in FPV drone videography. With ${experience} years of experience, we deliver stunning shots for commercial, real estate, and action sports videos. Our passion and cutting-edge techniques push the boundaries of what's possible. Choose Dew Aerials and take your vision to new heights.`
+            : `Witaj w Dew Aerials, specjaliści w dziedzinie wideofilmowania za pomocą dronów FPV. Posiadamy ${experience}-letnie doświadczenie w branży, dostarczając oszałamiające ujęcia dla filmów komercyjnych, nieruchomości oraz sportów ekstremalnych. Nasza pasja oraz innowacyjne techniki pozwalają nam przekraczać granice możliwości. Wybierz Dew Aerials i osiągnij nowe wysokości.`}
         </p>
       </TextWrapper>
     </PageWrapper>
