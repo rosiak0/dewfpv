c3fd0c2733d4cbaae15ee931b48977b424a197db
Dew FPV <maciejrosa1@gmail.com>
2023-05-22 12:01:41 +0200

    added animation

--- components/TextWrapper.tsx
+++ components/TextWrapper.tsx
@@ -1,8 +1,17 @@
+import { AnimatePresence, motion as m } from "framer-motion";
+
 const TextWrapper = ({ children }: any) => {
   return (
-    <div className="lg:w-3/5 lg:mx-auto mx-3 pt-20 pb-20 h-screen ">
-      <div className="mx-auto sm:mx-0 text-center ">{children}</div>
-    </div>
+    <AnimatePresence>
+      <m.div
+        initial={{ opacity: 0, y: -30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeOut", duration: 0.3 }}
+        className="lg:w-3/5 lg:mx-auto mx-3 pt-20 pb-20 h-screen "
+      >
+        <div className="mx-auto sm:mx-0 text-center">{children}</div>
+      </m.div>
+    </AnimatePresence>
   );
 };
 
